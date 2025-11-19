4a51deacf61125df49d02b0ec3aeea6daa9c5421
Bestin Joseph <bestin2maria@gmail.com>
2025-11-19 11:37:53 +0300

    updated 20251119

---
+++ api/src/app.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Post, Get, UseGuards, Request } from '@nestjs/common';
+import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthService } from './auth/auth.service';
+
+@Controller()
+export class AppController {
+  constructor(private authService: AuthService) {}
+
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @Get('/')
+  async home(@Request() req) {
+    return { msg: "welcome to jungle!!!!"}
+  }
+}
---
+++ build/122.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkhost=self.webpackChunkhost||[]).push([[122],{2122:(t,n,e)=>{function r(){return r=Object.assign?Object.assign.bind():function(t){for(var n=1;n<arguments.length;n++){var e=arguments[n];for(var r in e)Object.prototype.hasOwnProperty.call(e,r)&&(t[r]=e[r])}return t},r.apply(this,arguments)}e.d(n,{Z:()=>r})}}]);
